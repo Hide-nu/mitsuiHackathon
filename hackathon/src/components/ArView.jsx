b19799ee40a53847c93407a46549bf4aed53380a
Hide-nu <nkret805worm@gmail.com>
2021-10-23 19:33:34 +0900

    UPDATE: for ios

--- hackathon/src/components/ArView.jsx
+++ hackathon/src/components/ArView.jsx
@@ -4,17 +4,34 @@ export const ArView = () => {
   const [accelerationX, setAccelerationX] = useState(0);
   const [accelerationY, setAccelerationY] = useState(0);
   const [accelerationZ, setAccelerationZ] = useState(0);
-
+  console.log(!DeviceMotionEvent.requestPermission)
+  const deviceMotionRequest = () => {
+    if (DeviceMotionEvent.requestPermission) {
+      DeviceMotionEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === 'granted') {
+            window.addEventListener("devicemotion", (event) => {
+              if (!event.accelerationIncludingGravity) {
+                alert('event.accelerationIncludingGravity is null');
+                return;
+              }
+              setAccelerationX(event.accelerationIncludingGravity.x)
+              setAccelerationY(event.accelerationIncludingGravity.y)
+              setAccelerationZ(event.accelerationIncludingGravity.z)
+            })
+          }
+        })
+        .catch(console.error);
+    } else {
+      alert('DeviceMotionEvent.requestPermission is not found')
+    }
+  }
   useEffect(() => {
-    window.addEventListener("devicemotion", (event) => {
-      console.log(event)
-      setAccelerationX(event.acceleration.x)
-      setAccelerationY(event.acceleration.y)
-      setAccelerationZ(event.acceleration.z)
-    })
+    deviceMotionRequest()
   },[])
   return (
     <div>
+      <div>{!DeviceMotionEvent.requestPermission}</div>
       <span>{accelerationX}</span>
       <span>{accelerationY}</span>
       <span>{accelerationZ}</span>
