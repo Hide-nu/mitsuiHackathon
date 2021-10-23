09c9b149fcc6579f415a017ef5f5b0599b303bab
Hide-nu <nkret805worm@gmail.com>
2021-10-23 16:19:45 +0900

    UPDATE: webcam library

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam"
 
 function App() {
+  const videoConstraints = {
+    facingMode: { exact: "environment" }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Webcam videoConstraints={videoConstraints}/>
     </div>
   );
 }
