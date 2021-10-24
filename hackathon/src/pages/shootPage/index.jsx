a3c092b10146ba70f96e62a89b51b30901343122
Hide-nu <nkret805worm@gmail.com>
2021-10-24 10:37:15 +0900

    cant fix

--- hackathon/src/pages/shootPage/index.jsx
+++ hackathon/src/pages/shootPage/index.jsx
@@ -3,59 +3,61 @@ import { useState, useEffect, useRef, useCallback } from 'react';
 import Button from '@mui/material/Button';
 
 const ShootPage = () => {
-  const [lat, setLat] = useState(0.00);
-  const [lon, setLon] = useState(0.00);
-  const success = useCallback((pos) => {
-    console.log(pos)
-    setLat(pos.coords.latitude)
-    setLon(pos.coords.longtitude)
-  }, [lat, lon])
-  const error = useCallback((e) => console.error(e),[]);
+  // const [lat, setLat] = useState(0.00);
+  // const [lon, setLon] = useState(0.00);
+  // const success = (pos) => {
+  //   console.log(pos)
+  //   setLat(pos.coords.latitude)
+  //   setLon(pos.coords.longitude)
+  // }
+  // const error = useCallback((e) => console.error(e),[]);
 
-  useEffect(() => {
-    window.AFRAME.registerComponent('gpsPosition', {
-      init: function(){
-        navigator.geolocation.getCurrentPosition(success, error)
-        console.log("called");
-        const gpsPosition = this.el.sceneEl;
-        gpsPosition.addEventListener('gps-camera-update-positon', (event) => {
-          setLat(event.detail.position.latitude)
-          setLon(event.detail.position.longtitude)
-        });
-      },
-      update: function(){
-        console.log("called update");
-        const gpsPosition = this.el.sceneEl;
-        gpsPosition.addEventListener('gps-camera-update-positon', (event) => {
-          setLat(event.detail.position.latitude)
-          setLon(event.detail.position.longtitude)
-        });
-      }
-    });
-    document.querySelector('a-entity').setAttribute('gpsPosition', '');
-  },[])
-  console.log(lon)
+  // useEffect(() => {
+  //   window.AFRAME.registerComponent('gpsPosition', {
+  //     init: function(){
+  //       navigator.geolocation.getCurrentPosition(success, error);
+  //       console.log("called");
+  //       const gpsPosition = this.el.sceneEl;
+  //       gpsPosition.addEventListener('ggps-entity-place-update-positon', (event) => {
+  //         console.log("called gps init")
+  //         setLat(event.detail.position.latitude)
+  //         setLon(event.detail.position.longitude)
+  //       });
+  //     }
+  //   });
+  //   document.querySelector('a-text').setAttribute('gpsPosition', true);
+  // },[])
+  // console.log(lon)
   return (
-    <div>
-      <Button color="primary" size="large">{lat} {lon}</Button>
-    <a-scene
-      vr-mode-ui="enabled: false"
-      embedded
-      arjs="sourceType: webcam; debugUIEnabled: false;"
-    >
-       <a-box
-        material="color: red"
+     <div>
+        {/*<Button color="primary" size="large">{lon} {lon}</Button>
+      <div>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs="sourceType: webcam; debugUIEnabled: false;"
+      >
+         <a-box
+          material="color: red"
+          gps-entity-place="latitude: 35.687977; longitude: 139.7723483;"
+          scale="30 30 30"
+        ></a-box>
+        <a-box
+          material="color: red"
+          gps-entity-place={`latitude: ${lat}; longitude: ${lon};`}
+          scale="30 30 30"
+        ></a-box>
+        <a-text
+        value="This content will always face you."
+        look-at="[gps-camera]"
+        scale="50 50 50"
+        position="0 50 0"
         gps-entity-place={`latitude: ${lat}; longitude: ${lon};`}
-        scale="30 30 30"
-      ></a-box>
-      <a-entity
-        scale="300 300 300"
-        position="0 55 0"
-        gps-entity-place={`latitude: ${lat}; longitude: ${lon};`}
-      />
-      <a-camera gps-camera rotation-reader> </a-camera>
-    </a-scene>
-    </div>
+      ></a-text>
+        <a-camera gps-camera="minDistance:30; maxDistance:100 zoom:0.5" rotation-reader> </a-camera>
+      </a-scene>
+  </div>*/}
+     </div>
   )
 }
 
